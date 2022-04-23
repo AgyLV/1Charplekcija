1305180ff6f09edd64dba728c9f5693a7159fa91
agnese.veze <agnese.veze@gmail.com>
2022-04-23 21:49:38 +0300

    3. uzdevums

--- App2/3MajasDarbs/3MajasDarbs/Prece.cs
+++ App2/3MajasDarbs/3MajasDarbs/Prece.cs
@@ -2,5 +2,35 @@
 {
     internal class Prece
     {
+        public Prece(string name)
+        {
+            Nosaukums = name;
+        }
+        public string Nosaukums { get; set; }
+
+        public double Garums { get; set; }
+
+        public double Platums { get; set; }
+
+        public double Dimensija { get; set; }
+
+        public double Svars { get; set; }
+
+        public double Cena{ get; set; }
+
+
+
+
+        public void Precesinfo()
+        {
+            Console.WriteLine($"Prece {Nosaukums}");
+            Console.WriteLine($"Preces garums: {Garums} cm");
+            Console.WriteLine($"Preces platums: {Platums} cm");
+            Console.WriteLine($"Preces dziļums: {Dimensija} cm");
+            Console.WriteLine($"Preces svars: {Svars} kg");
+            Console.WriteLine($"Preces cena: {Cena} Eur");
+        }
     }
+
+
 }
--- App2/3MajasDarbs/3MajasDarbs/Program.cs
+++ App2/3MajasDarbs/3MajasDarbs/Program.cs
@@ -32,3 +32,43 @@ car.Speed = 50;
 
 
 car.BeepBeep();
+
+Console.WriteLine();
+Console.WriteLine("=================================");
+Console.WriteLine();
+//3. uzdevums
+
+string name1 = "Termo krūze";
+Prece prece1 = new Prece(name1);
+
+prece1.Garums = 15.8;
+prece1.Platums = 7.9;
+prece1.Dimensija = 7.9;
+prece1.Svars = 0.2;
+prece1.Cena = 9.99;
+
+prece1.Precesinfo();
+
+string name2 = "Ūens pudele";
+Prece prece2 = new Prece(name2);
+
+prece2.Garums = 20.9;
+prece2.Platums = 12.2;
+prece2.Dimensija = 12.2;
+prece2.Svars = 0.5;
+prece2.Cena = 12.45;
+
+Console.WriteLine();
+prece2.Precesinfo();
+
+string name3 = "Alumīnija krūze ovāla";
+Prece prece3 = new Prece(name3);
+
+prece3.Garums = 12.6;
+prece3.Platums = 6.0;
+prece3.Dimensija = 4.0;
+prece3.Svars = 0.3;
+prece3.Cena = 4.55;
+
+Console.WriteLine();
+prece3.Precesinfo();
