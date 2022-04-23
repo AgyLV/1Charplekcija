2997b343ca4784636868ea91c8d0654824fc0ad6
agnese.veze <agnese.veze@gmail.com>
2022-04-23 19:48:23 +0300

    majas darbs

--- App2/3MajasDarbs/3MajasDarbs/Telefons.cs
+++ App2/3MajasDarbs/3MajasDarbs/Telefons.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace _3MajasDarbs
 {
-    internal class Telefons
+    public class Telefons
     {
+        public Telefons(string name)
+        {
+            Marka = name;
+        }
+
+        public string Marka { get; set; }
+
+        public double Scale1 { get; set; }
+
+        public string Modelis { get; set; }
+
+        public string Zvana { get; set; }
+
+        public string SMS { get; set; }
+
+        public void Informacija()
+        {
+            Console.WriteLine($"Labdien! Telefons {Marka} {Modelis} ir {Scale1} liels.");
+        }
     }
+
+        public void VaiZvans()
+        {
+        Console.WriteLine("telefonam ir zvana funkcija.");
+        }
+
+        public void VaiZina()
+        {
+        Console.WriteLine("Telefonam ir SMS fukcija.");
+        }
+    
 }
